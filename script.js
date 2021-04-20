3ee52834135164607415f2148c54dd90b9c97ac9
Pranjal Verma <pvcodes@gmail.com>
2021-04-20 21:04:43 +0530

    version 2.1

---
+++ script.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+var el = document.getElementById("presentation");
+var cursore = document.getElementById("cursor");
+cursore.style.visibility="hidden";
+el.style.visibility="visible";
+}, 3000);
