5bd58c832a3cde9ab5ccb06a6645693db2123af5
Pranjal Verma <pvcodes@gmail.com>
2022-01-11 17:08:03 +0530

    Added: current update

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 setTimeout(() => {
-var el = document.getElementById("presentation");
-var cursore = document.getElementById("cursor");
-cursore.style.visibility="hidden";
-el.style.visibility="visible";
+    var el = document.getElementById("presentation");
+    var cursore = document.getElementById("cursor");
+    cursore.style.visibility = "hidden";
+    el.style.visibility = "visible";
 }, 3000);
+
+window.onload = () => {
+    setTimeout(
+        () => {
+            document.getElementById('popup').style.opacity = 0;
+        }, 5000);
+};
